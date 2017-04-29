edb6a52bc9ace3f0c6c5ffdbc74dbed8030ae601
NajlaAlariefy <najlaalariefy@gmail.com>
2017-04-29 14:03:45 +1000

    Cleaned server interactions

--- src/server/serverInteractions.java
+++ src/server/serverInteractions.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package server;
 
 import java.io.DataInputStream;
@@ -23,21 +19,13 @@ import java.util.logging.Level;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
-/**
- *
- * @author najla
- */
+ 
 public class serverInteractions {
 
 public void exchange() throws IOException {
 
-        //Pick a random server to connect from the list
-        if (Server.serverRecords.isEmpty()) {
-            System.out.println("server interactions  empty");
-        } else {
-            
-            //IF the server contains itself in the serverrecords list, then remove it
+    
+        //IF the server contains itself in the serverrecords list, then remove it
             JSONObject serverTraverser = new JSONObject();
             for (int i = 0; i < Server.serverRecords.size(); i++) {
                 serverTraverser = (JSONObject) Server.serverRecords.get(i);
@@ -49,15 +37,22 @@ public void exchange() throws IOException {
 
                 }
             }
-
+            
+            
+            
+        if (Server.serverRecords.isEmpty()) {
+            Server.debug("INFO", "server exchange initiated: empty server record list.");
+        } else {
+            
+            
+            
+            //Pick a random server to connect from the list
+            Server.debug("INFO", "server exchange initiated: randomly selecting a server");
             Random r = new Random();
             JSONObject randomServer = new JSONObject();
-            System.out.println("server interactions  working");
             int size = Integer.valueOf(Server.serverRecords.size());
             int index = Integer.valueOf(r.nextInt(size));
-
             randomServer = (JSONObject) Server.serverRecords.get(index);
-            //serverList[r.nextInt(serverList.length)];
             String connect_host = randomServer.get("hostname").toString();
             int connect_port = ((Long) randomServer.get("port")).intValue();
 
@@ -67,21 +62,20 @@ public void exchange() throws IOException {
                 socket = new Socket(connect_host, connect_port);
                 String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
                 System.out.println(time + " - [INFO] - exchange with " + connect_host + ":" + connect_port + " is successful.");
+                
                 // Sending the list to the randomly selected server
+                // Receiving ackowledgement (success)/(error)                
                 JSONObject listToRandomServer = new JSONObject();
                 listToRandomServer.put("command", "EXCHANGE");
                 listToRandomServer.put("serverList", Server.serverRecords);
                 DataOutputStream serverOutput = new DataOutputStream(socket.getOutputStream());
-                output(listToRandomServer, serverOutput);
-
-                
-                
+                serverOutput.writeUTF(listToRandomServer.toJSONString());
+                Server.debug("SEND EXCHANGE", listToRandomServer.toJSONString());
                 DataInputStream serverInput = new DataInputStream(socket.getInputStream());
                 String message =  serverInput.readUTF();
                 JSONParser parser = new JSONParser();
                 JSONObject JSONresponse = (JSONObject) parser.parse(message);
-                time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
-                System.out.println(time + " - [RECEIVE exchange] - " + JSONresponse.toJSONString());
+                Server.debug("RECEIVE EXCHANGE",  JSONresponse.toJSONString());
                 
                 
                 // Display distinct servers in the serverRecords
@@ -91,15 +85,17 @@ public void exchange() throws IOException {
                 socket.close();
             } catch (Exception e) {
                 // If the connection with the random server is not established
-                String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
-                System.out.println(time + " - [INFO] -  connection with server " + connect_host + ":" + connect_port + " was not successful: " + e);
+                Server.debug("INFO","connection with server " + connect_host + ":" + connect_port + " was not successful. ");
+                Server.debug("INFO","error message : "  + e);
+               
                 serverTraverser = new JSONObject();
 
                 for (int i = 0; i < Server.serverRecords.size(); i++) {
                     serverTraverser = (JSONObject) Server.serverRecords.get(i);
                     if (serverTraverser.get("hostname").equals(connect_host) && serverTraverser.get("port").equals(connect_port)) {
                         Server.serverRecords.remove(i);
-
+                Server.debug("INFO","server has been removed");
+               
                     }
                 }
             } finally {
@@ -107,24 +103,5 @@ public void exchange() throws IOException {
             }
         }
     }
-
-    private void output(JSONObject response, DataOutputStream output) throws IOException {
-        Boolean debug = true;
-        System.out.println("SERVER INTERACTIONS UTF OUT");
-        if (debug) {
-            String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
-            System.out.println(time + " - [SEND] - " + response.toJSONString());
-            /*Handler consoleHandler = null;
-            consoleHandler = new ConsoleHandler();
-            LOGGER.addHandler(consoleHandler);
-            consoleHandler.setLevel(Level.ALL);
-            LOGGER.setLevel(Level.ALL);
-            // LOGGER.fine("[SEND]:" + response.toJSONString());
-             */
-
-        }
-
-        output.writeUTF(response.toJSONString());
-
-    }
+ 
 }
